2ed95d92f7c8ce51f8c6b1be285fe9bd586c8cd2
Alan Vinicius Silva <alanvinicios@gmail.com>
2021-04-30 19:09:54 -0300

    v 1.1.0

--- script.js
+++ script.js
@@ -6,33 +6,47 @@ const $untrimmedSVGContainer = document.querySelector('.untrimmed .svg-container
 const $trimmedSVGCode = document.querySelector('.trimmed .code')
 const $trimmedSVGContainer = document.querySelector('.trimmed .svg-container')
 
-$untrimmedSVGCode.addEventListener('input', (evt) => {
-	renderUntrimmedSVG()
-})
+$untrimmedSVGCode.addEventListener('input', trimSVG)
 
-function validateSVGCode() {
-	if ($untrimmedSVGCode.value.match(/^[\s]*<svg(.|\n)*<\/svg>[\s]*$/)) {
-		$untrimmedSVGCode.style.borderColor = 'green'
-		return true
-	} else {
-		$untrimmedSVGCode.style.borderColor = 'red'
-		return false
-	}
-}
-
-function renderUntrimmedSVG() {
-	if (validateSVGCode()) {
+function trimSVG() {
+	sanitizeInput()
+	try {
 		$untrimmedSVGContainer.innerHTML = $untrimmedSVGCode.value
-		renderTrimmedSVG()
+		const $untrimmedSVG = $untrimmedSVGContainer.firstChild
+		const bbox = $untrimmedSVG.getBBox()
+		const viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(' ')
+
+		const $trimmedSVG = $untrimmedSVG.cloneNode(true)
+		$trimmedSVG.setAttribute('viewBox', viewBox)
+		$trimmedSVG.removeAttribute('width')
+		$trimmedSVG.removeAttribute('height')
+		$trimmedSVGCode.value = $trimmedSVG.outerHTML
+		$trimmedSVGContainer.innerHTML = $trimmedSVG.outerHTML
+	} catch (e) {
+		console.log(e)
+		$trimmedSVGContainer.innerText = 'TRIMMED SVG PREVIEW'
+		$untrimmedSVGContainer.innerHTML = 'UNTRIMMED SVG PREVIEW'
+		$trimmedSVGCode.value = ''
 	}
 }
 
-function renderTrimmedSVG() {
-	const $untrimmedSVG = $untrimmedSVGContainer.firstChild
-	const $trimmedSVG = $untrimmedSVG.cloneNode(true)
-
-	const bbox = $untrimmedSVG.getBBox()
-	const viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(' ')
-	$trimmedSVG.setAttribute('viewBox', viewBox)
-	$trimmedSVGContainer.innerHTML = $trimmedSVG.outerHTML
+function sanitizeInput() {
+	// Tira tudo que estiver fora de <svg></svg>
+	const array = $untrimmedSVGCode.value.match(/((.|\n)*(<svg(.|\n)*>(.|\n)*<\/svg>))/)
+	$untrimmedSVGCode.value = array[3]
 }
+
+const $copyButton = document.querySelector('.copy-button')
+const $outputContainer = document.querySelector('.output-container')
+$copyButton.addEventListener('click', (evt) => {
+	if ($trimmedSVGCode.value != '') {
+		/* Select the text field */
+		$trimmedSVGCode.select()
+		$trimmedSVGCode.setSelectionRange(0, 99999) /* For mobile devices */
+
+		/* Copy the text inside the text field */
+		document.execCommand('copy')
+		$outputContainer.classList.add('copied')
+		setTimeout(() => $outputContainer.classList.remove('copied'), 5000)
+	}
+})
